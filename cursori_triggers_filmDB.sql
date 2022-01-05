5f853494e6728a113436d650ae872c5504d7e17d
Simona9629 <voinea.simona96@gmail.com>
2022-01-05 11:27:24 +0200

    adaugare triggers

--- cursori_triggers_filmDB.sql
+++ cursori_triggers_filmDB.sql
@@ -106,4 +106,73 @@ DELIMITER ;
 SELECT productii_dupa_membru('Stephen Edwin King');
 SELECT productii_dupa_membru('Christopher Edward Nolan');
 
+-- triggers
+
+-- inainte de a adauga un actor se fac prelucrarile:
+-- data de nastere a actorului e mai mare decat current date -> mesaj de eroare
+-- nume, prenume scrise capitalize
+
+DELIMITER //
+CREATE TRIGGER bi_actor BEFORE INSERT
+ON actor FOR EACH ROW
+BEGIN
+	SET NEW.nume = CONCAT(UCASE(LEFT(NEW.nume, 1)), LCASE(SUBSTRING(NEW.nume, 2)));
+    SET NEW.prenume = CONCAT(UCASE(LEFT(NEW.prenume,1)), LCASE(SUBSTRING(NEW.prenume,2)));
+    IF NEW.data_nasterii > CURDATE() THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Data nasterii este mai mare decat data curenta';
+    END IF;
+END;
+//
+DELIMITER ;
+
+INSERT INTO actor VALUES (NULL, 'lawrenCE', 'JeNNifer', '1990-08-15', 'Louisville', 1.75, NULL);
+-- DROP TRIGGER bi_actor;  
+INSERT INTO actor VALUES(NULL, 'anthony', 'LITTLE', '2022-02-02',NULL,NULL,NULL);
+
+-- actualizarea parolei unui utilizator
+-- daca e aceeasi parola se va trimite un mesaj de eroare
+-- lungimea parolei minim 10 caractere
+
+
+DELIMITER //
+CREATE OR REPLACE TRIGGER bu_utilizator BEFORE UPDATE
+ON utilizator FOR EACH ROW
+BEGIN
+	IF NEW.pass = OLD.pass THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Parola a fost deja asociata acestui cont.Va rugam introduceti o noua parola';
+    END IF;
+    IF CHAR_LENGTH(NEW.pass) < 10 THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Parola prea scurta. Noua parola trebuie sa contina minim 10 caractere';
+    END IF;
+END;
+//
+DELIMITER ;
+
+UPDATE utilizator SET pass = 'PassALEX' WHERE id = 5;
+UPDATE utilizator SET pass = 'PassA' WHERE id = 5;
+
+-- pentru un membru decedat se va actualiza tabela membru
+-- se adauga o noua coloana data_decesului in care se salveaza data 
+-- in tabela in_memoriam se va salva nume, prenume si data decesului
+
+CREATE TABLE in_memoriam(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nume VARCHAR(50) NOT NULL,
+    prenume VARCHAR(50) NOT NULL,
+    data_decesului DATE NOT NULL
+);
+ALTER TABLE membru
+ADD COLUMN data_decesului DATE;
+
+DELIMITER //
+CREATE OR REPLACE TRIGGER au_membru AFTER UPDATE
+ON membru FOR EACH ROW
+BEGIN
+	INSERT INTO in_memoriam VALUES(NULL, OLD.nume, OLD.prenume, NEW.data_decesului);
+END;
+//
+DELIMITER ;
+
+UPDATE membru SET data_decesului = '1999-03-07' WHERE id = 25;
+SELECT * FROM in_memoriam;
 
